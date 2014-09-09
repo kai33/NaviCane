2d7364506bd058842937eed7ff0cb67cf5afe2db
franklingu <franklingujunchao@gmail.com>
2014-09-09 16:02:20 +0800

    add testing for import

--- RPi/Positioning/triangulation.py
+++ RPi/Positioning/triangulation.py
@@ -1,6 +1,9 @@
-import os, sys
+import os
+import sys
 
+# for running this script in shell only
 if __name__ == '__main__':
-    sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+    sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                 os.pardir))
 
 from Communication.WiFi import internet_access
