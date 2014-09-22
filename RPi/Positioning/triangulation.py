4b568cc8b3ff929b430a65a3c90401b1d7e8b8fe
franklingu <franklingujunchao@gmail.com>
2014-09-22 21:54:22 +0800

    adding to wifi positioning

--- RPi/Positioning/triangulation.py
+++ RPi/Positioning/triangulation.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import math
 from time import sleep
 
 # for running this script in shell only
@@ -11,19 +12,23 @@ from Communication.WiFi import access_points_scan
 from Communication.WiFi import internet_access
 
 
-class WiFiRSSIPositioning(object):
+class WiFiPositioning(object):
     """
     Gathering of WiFi access points information and provide a rough
     estimation of current user's position
     """
     def __init__(self, building, level):
-        super(WiFiRSSIPositioning, self).__init__()
+        super(WiFiPositioning, self).__init__()
         self._building = building
         self._level = level
         self._scan_results = {}
+        self._access_points = {}  # macAddr: info
         self._get_access_points()
 
     def _get_access_points(self):
+        """
+        download access points information from server. cache that afterwards
+        """
         access_points = []
         while not access_points:
             floor_plan = internet_access.get_floor_plan(self._building,
@@ -36,6 +41,17 @@ class WiFiRSSIPositioning(object):
         self._update_scan_results()
         # triangulation part
 
+    def _get_distance(self, signal_level, frequency):
+        """
+        Making use of free space path loss theory. Not working well with
+        obstacles blocking the signal. needs improvement
+
+        signal_level: (dB)
+        frequency: (MHz)
+        """
+        exp = (27.55 - 20 * math.log10(frequency) + math.fabs(signal_level)) / 20.0
+        return 10 ** exp
+
     def _update_scan_results(self):
         for i in xrange(1, 5):
             temp_scanned_results = access_points_scan.get_scan_results()
@@ -44,4 +60,4 @@ class WiFiRSSIPositioning(object):
                     pass  # update result
                 else:
                     self._scan_results[sr.get_mac_addr()] = sr
-            sleep(0.1)
+            sleep(0.01)
