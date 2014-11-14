cc00af75d308909874b964d836d2614a6519fcf4
Kai33 <gigikie@gmail.com>
2014-11-14 15:03:06 +0800

    .

--- RPi/Navigation/special_node.py
+++ RPi/Navigation/special_node.py
@@ -5,8 +5,7 @@ class SpecialNode:
         'com2-2-6',  # COM2-2 junction towards stairwell
         'com2-2-13',  # another door, just bf stairwell
         'com2-2-14',  # start of stairwell (lvl2)
-        'com2-3-10',  # end of stairwell (lvl3)
-        'com2-3-11'  # COM2 lvl2~lvl3 connection
+        'com2-3-10'  # end of stairwell (lvl3)
     }
 
     @classmethod
