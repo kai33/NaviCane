290526ba393c5ff2fb9e4c6d04ef2b3811acda1a
Kai33 <gigikie@gmail.com>
2014-11-14 22:11:24 +0800

    update special nodes

--- RPi/Navigation/special_node.py
+++ RPi/Navigation/special_node.py
@@ -3,13 +3,13 @@ class SpecialNode:
         # 'com1-2-24',  # in front of seminar room 11
         # 'com1-2-26',  # bf COM1~COM2 connection
         'com1-2-18',  # in front of tr11
-        'com1-2-34',
-        'com1-2-31',
-        'com2-2-5', #colin's office
+        'com1-2-34',  # in front of tr5
+        'com1-2-31',  # connect COM1 and COM2
+        'com2-2-5',  # Colin's office
         'com2-2-6',  # COM2-2 junction towards stairwell
-        'com2-2-13',  # another door, just bf stairwell
         'com2-2-14',  # start of stairwell (lvl2)
         'com2-3-10',  # end of stairwell (lvl3)
+        'com2-3-3',  # level3 towards another path
         'com2-3-14'  # henry's office
     }
 
